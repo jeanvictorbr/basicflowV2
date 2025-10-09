062dd50453fa55923b305aee305e4bf442cb9420
jeanvictorbr <jeanvictorbr@gmail.com>
2025-10-09 17:35:00 -0300

    Update select_registros_cargo_aprovado.js

--- handlers/selects/select_registros_cargo_aprovado.js
+++ handlers/selects/select_registros_cargo_aprovado.js
@@ -1,4 +1,4 @@
-// handlers/selects/select_registros_cargo_aprovado.js
+// Substitua em: handlers/selects/select_registros_cargo_aprovado.js
 const db = require('../../database.js');
 const generateRegistrosMenu = require('../../ui/registrosMenu.js');
 
@@ -10,7 +10,15 @@ module.exports = {
     async execute(interaction) {
         const roleId = interaction.values[0];
         await db.query(`UPDATE guild_settings SET registros_cargo_aprovado = $1 WHERE guild_id = $2`, [roleId, interaction.guild.id]);
+        
         const settingsResult = await db.query('SELECT * FROM guild_settings WHERE guild_id = $1', [interaction.guild.id]);
-        await interaction.update({ components: generateRegistrosMenu(settingsResult.rows[0]), flags: V2_FLAG | EPHEMERAL_FLAG });
+        
+        // CORREÇÃO: Garante que um objeto vazio seja passado se não houver configurações.
+        const menu = await generateRegistrosMenu(interaction, settingsResult.rows[0] || {});
+
+        await interaction.update({ 
+            components: menu, 
+            flags: V2_FLAG | EPHEMERAL_FLAG 
+        });
     }
 };
