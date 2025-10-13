297c43bb838064c1b6b6d039757d20424d105601
jeanvictorbr <jeanvictorbr@gmail.com>
2025-10-12 23:07:42 -0300

    Update select_store_vitrine_category.js

--- handlers/selects/select_store_vitrine_category.js
+++ handlers/selects/select_store_vitrine_category.js
@@ -9,7 +9,7 @@ module.exports = {
         const selectedCategoryId = interaction.values[0];
 
         const settings = (await db.query('SELECT * FROM guild_settings WHERE guild_id = $1', [interaction.guild.id])).rows[0] || {};
-        const categories = (await db.query('SELECT * FROM store_categories WHERE guild_id = $1 ORDER BY price ASC', [interaction.guild.id])).rows;
+        const categories = (await db.query('SELECT * FROM store_categories WHERE guild_id = $1 ORDER BY name ASC', [interaction.guild.id])).rows;
         const products = (await db.query('SELECT * FROM store_products WHERE guild_id = $1 AND is_enabled = true', [interaction.guild.id])).rows;
         
         const vitrinePayload = generateVitrineMenu(settings, categories, products, selectedCategoryId, 0);
